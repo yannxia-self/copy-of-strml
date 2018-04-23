de6a19dd53f209f85690c7e33c9123250bf73e50
yann <me.yan.xia@qq.com>
2018-04-23 12:10:11 +0800

    update

--- src/Mobile.vue
+++ src/Mobile.vue
@@ -38,39 +38,51 @@
 }
 /* 白色背景太单调了，我们来点100offer的logo标志色 */
 html {
-  color: rgb(24,24,48); background: rgb(24,192,168);
+  color: rgb(222,222,222);
+  background: rgb(0,43,54);
 }
 /* 文字离边框太近了 */
 .styleEditor {
   padding: .5em;
   border: 1px solid;
-  margin: .5em;
   overflow: auto;
-  width: 45vw; height: 90vh;
+  width: 90vw;
+  margin: 2.5vh 5vw;
+  height: 90vh;
 }
-/* 来个代码高亮更专业 */
-.token.selector{ color: rgb(133,153,0); }
-.token.property{ color: rgb(187,137,0); }
-.token.punctuation{ color: yellow; }
-.token.function{ color: rgb(42,161,152); }
+/* 太高了 */
+.styleEditor {
+  height: 45vh;
+}
+/* 代码高亮 */
+.token.selector{
+  color: rgb(133,153,0);
+}
+.token.property{
+  color: rgb(187,137,0);
+}
+.token.punctuation{
+  color: yellow;
+}
+.token.function{
+  color: rgb(42,161,152);
+}
 
-/* 100offer 3D 性能优秀，咱们也加点 3D 效果呗 */
+/* 加点 3D 效果呗 */
 html{
   perspective: 1000px;
 }
 .styleEditor {
   position: fixed; left: 0; top: 0;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
-          transform: rotateY(10deg) translateZ(-100px) ;
+  transform: rotateX(-10deg) translateZ(-50px) ;
 }
 
-/* 100offer 的IDE是灰常好用的，也来一个编辑器 */
+/* 接下来我给自己准备一个编辑器 */
 .resumeEditor{
-  position: fixed; right: 0; top: 0;
-  padding: .5em;  margin: .5em;
-  width: 48vw; height: 90vh;
+  position: fixed;
+  top: 50%; left: 0;
+  padding: .5em;  margin: 2.5vh;
+  width: 95vw; height: 45vh;
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
